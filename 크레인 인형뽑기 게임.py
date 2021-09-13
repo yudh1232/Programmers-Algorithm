13bdc1f91da14e56e17bf028660d9cefea51eeb8
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-09-14 00:54:04 +0900

    Create 크레인 인형뽑기 게임.py

---
+++ 크레인 인형뽑기 게임.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+def solution(board, moves):
+    # 정사각 격자의 크기
+    n = len(board)
+
+    # 열 별로 큐를 만들어서 관리
+    q_list = [deque() for i in range(n)]
+
+    # board를 위에서부터 살피며 열 별로 큐를 채움
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] != 0:
+                q_list[j].append(board[i][j])
+
+    # basket은 스택으로 관리
+    basket = []
+
+    # 결과값
+    answer = 0
+
+
+    for m in moves:
+        # 큐가 비어있으면 continue
+        if len(q_list[m - 1]) == 0:
+            continue
+        # 아니면 인형을 빼냄    
+        else:
+            item = q_list[m - 1].popleft()
+        
+        # basket이 비어있으면 인형을 넣음
+        if len(basket) == 0:
+            basket.append(item)
+        # 비어있지 않으면
+        else:
+            top = basket[-1]
+            
+            # top과 인형을 바교해서 같으면
+            if top == item:
+                basket.pop()
+                answer += 2
+            # 다르면
+            else:
+                basket.append(item)
+
+    # 결과 리턴
+    return answer
